ccb38eb6b928ef9255528d2fc7aff568cd993291
NickM20 <68336370+NickM20@users.noreply.github.com>
2021-12-09 01:55:48 -0500

    added lessons

---
+++ if.js
@@ -0,0 +1,19 @@
+// Hello class, today you are going to create a number 
+// to letter grade conversion
+
+// Declare thevariable grade here
+
+
+// 90-100 grade ramge
+
+
+// 80-89 grade ramge
+
+
+// 70-79 grade ramge
+
+
+// 65 - 69 grade range
+
+
+// Grade bellow 60
